3dc510083c538a36b147aeae2a25c353add43556
Dhanush123 <dhanush.patel@ymail.com>
2016-08-26 22:47:14 -0700

    hopefully this works
    spent a lot of time and effort to address the most recent reasons for
    failing to get certified

--- index.js
+++ index.js
@@ -24,58 +24,80 @@ exports.handler = function(event, context, callback) {
 };
 
 var handlers = {
-    'NewSession': function () {
+    'LaunchRequest': function () {
         console.log("went in newsession function");
-        this.emit('GetDistance');
-    },
-    'GetDistance': function () {
-        console.log("went in getdistance function");
+//        this.emit('GetDistance');
         
-        if(this.event.request.intent!=undefined && this.event.request.intent!=undefined){
-            
-            var distSlot1 = this.event.request.intent.slots.Start.value; 
-            var distSlot2 = this.event.request.intent.slots.Finish.value; 
-            
-            console.log(distSlot1+"-->"+distSlot2); 
-
-            var self = this;
-            var params = {
-            origins: distSlot1,
-            destinations: distSlot2,
-            units: 'imperial'
-            };
-            gmAPI.distance(params, function(err, result){
-            console.log("err: "+err);
-            console.log("result: "+result);
-            console.log("is result ok: "+result.status);
-                
-            var arry = result.rows[0].elements;
-                
-            if(arry[0].distance==undefined || arry[0].duration==undefined){
-                self.emit('Unhandled'); 
-            }
-            else{
-                console.log("distance is: "+arry[0].distance.text);  
-                console.log("time is: "+arry[0].duration.text);   
-                var totalResult = "The distance from " + params.origins + " to " + params.destinations + " is " + arry[0].distance.text + " and will take approximately " + arry[0].duration.text;
-
-                self.emit(':tell',totalResult);
-            }
-
-          });
-        }
-        else{
         // If the user either does not reply to the welcome message or says something that is not
         // understood, they will be prompted again with this text.
         this.attributes['speechOutput'] = 'Welcome to ' + SKILL_NAME + '. You can ask a question like, what is the ' + 'distance from San Francisco to New York City? Please tell me two cities you would like to find a driving distance between.';
 
-        this.attributes['repromptSpeech'] = 'To find a driving distance, say something like: what is the distance from City 1 to City 2?';
+        this.attributes['repromptSpeech'] = 'To find a driving distance, say something like: what is the distance from San Diego to Los Angeles?';
+        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
+    
+    },
+    'GetDistance': function () {
+        console.log("went in getdistance function");
+        
+        if(this.event.request.intent.slots.Start.value!=undefined && this.event.request.intent.slots.Finish.value!=undefined){
+            var self = this;
+            var distSlot1 = this.event.request.intent.slots.Start.value; 
+            var distSlot2 = this.event.request.intent.slots.Finish.value; 
+            if(distSlot1==undefined || distSlot2==undefined){
+                self.emit('Unhandled')
+            }
+            else{
+                console.log(distSlot1+"-->"+distSlot2); 
+                var params = {
+                origins: distSlot1,
+                destinations: distSlot2,
+                units: 'imperial'
+                };
+
+                gmAPI.distance(params, function(err, result){
+                console.log("err: "+err);
+                console.log("result: "+result);
+                console.log("is result ok: "+result.status);
+
+                var arry = result.rows[0].elements;
+
+                if(arry[0].distance==undefined || arry[0].duration==undefined){
+                    self.emit('Unhandled'); 
+                }
+                else{
+                    console.log("distance is: "+arry[0].distance.text);  
+                    console.log("time is: "+arry[0].duration.text);   
+                    var totalResult = "The distance from " + params.origins + " to " + params.destinations + " is " + arry[0].distance.text + " and will take approximately " + arry[0].duration.text;
+
+                    self.emit(':tell',totalResult);
+                }
+
+              });
+            }
+        }
+        else if((this.event.request.intent.slots.Start.value==undefined && this.event.request.intent.slots.Finish.value!=undefined) || (this.event.request.intent.slots.Start.value!=undefined && this.event.request.intent.slots.Finish.value==undefined)){
+                console.log("only one of two cities defined logic");
+                this.emit('Unhandled');   
+        }
+        else if(this.event.request.intent.slots.Start.value =="help" || this.event.request.intent.slots.Finish.value =="help"){
+                console.log("help if logic");
+                this.emit('HelpMe');
+        }
+        else{
+        // If the user either does not reply to the welcome message or says something that is not
+        // understood, they will be prompted again with this text.
+        this.attributes['speechOutput'] = 'Welcome to ' + SKILL_NAME + '. You can ask a question like, what is the ' + 'distance from Austin to Dallas? Please tell me two cities you would like to find a driving distance between.';
+
+        this.attributes['repromptSpeech'] = 'To find a driving distance, say something like: what is the distance from Berkeley to Palo Alto?';
         this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])  
         }
     },
     'AMAZON.HelpIntent': function() {
+        console.log("went in Amazon.HelpIntent");
         // If the user either does not reply to the welcome message or says something that is not
         // understood, they will be prompted again with this text.
+        this.attributes['speechOutput'] = 'You can ask a question like, what is the ' +
+            'distance from Seattle to Portland? Please tell me two cities you would like to find a driving distance between.';
         this.attributes['repromptSpeech'] = 'You can ask a question like, what is the ' +
             'distance from Seattle to Portland? Please tell me two cities you would like to find a driving distance between.';
         this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
@@ -92,5 +114,13 @@ var handlers = {
     'Unhandled': function() {
         this.emit(':tell', 'Sorry, I was unable to understand and process your request. Please try again.');
         this.emit('SessionEndedRequest');
+    },
+    'HelpMe': function() {
+        console.log("went in HelpMe");
+        this.attributes['speechOutput'] = 'You can ask a question like, what is the ' +
+            'distance from Atlanta to Boston? Please tell me two cities you would like to find a driving distance between.';
+        this.attributes['repromptSpeech'] = 'You can ask a question like, what is the ' +
+            'distance from Atlanta to Boston? Please tell me two cities you would like to find a driving distance between.';
+        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
     }
 };
